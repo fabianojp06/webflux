dd21fed596b38f102e135378cd9a486736864b74
Fabiano Garcia <fabianojp06@gmail.com>
2023-09-14 12:56:08 -0300

    Start project

---
+++ src/main/java/br/com/fabiano/webflux/WebfluxCursoApplication.java
@@ -0,0 +1,14 @@
+package br.com.fabiano.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxCursoApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/br/com/fabiano/webflux/WebfluxCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabiano.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
